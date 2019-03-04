7149753407b45570e04e8c7c45f0cba479e53503
ekhan3119 <ekc3119@gmail.com>
2019-03-04 13:14:57 -0500

    created api routes, html routes, post api routes.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
-
-// Requiring our models for syncing
-//var db = require("./models");
+var axios = require('axios');
+var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,13 +15,13 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-/* require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app); */
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-//db.sequelize.sync({}).then(function() {
-app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+db.sequelize.sync({}).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
 });
-//});
