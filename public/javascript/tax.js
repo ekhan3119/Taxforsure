da6c56c2e5c410c5e9c29377e36e31512e7d8dcf
Lisa Stasio <lstasio@sgintl.com>
2019-03-08 13:30:20 -0500

    updated date format to fit inside table columns

--- public/javascript/tax.js
+++ public/javascript/tax.js
@@ -7,37 +7,37 @@ $(function() {
   $('input[name="daterange"]').daterangepicker({
     opens: 'left'
   }, function(start, end, label) {
-    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    console.log(start.format('YYYY-MM-DD'));
+    console.log("A new date selection was made: " + start.format('MM-DD') + ' to ' + end.format('MM-DD'));
+    console.log(start.format('MM-DD'));
 
 
-    var myDate = (start.format('YYYY-MM-DD'));
+    var myDate = (start.format('MM-DD'));
     var weekDayName =  moment(myDate).format('dddd');
     console.log(weekDayName);
 
      
-    var tues = moment(myDate, 'YYYY-MM-DD');
-    var thing2 = tues.add(`1`, 'days').format('YYYY-MM-DD');
+    var tues = moment(myDate, 'MM-DD');
+    var thing2 = tues.add(`1`, 'days').format('MM-DD');
     console.log(thing2);
 
-    var wed = moment(myDate, 'YYYY-MM-DD');
-    var thing3 = wed.add(`2`, 'days').format('YYYY-MM-DD');
+    var wed = moment(myDate, 'MM-DD');
+    var thing3 = wed.add(`2`, 'days').format('MM-DD');
     console.log(thing3);
 
-    var thurs = moment(myDate, 'YYYY-MM-DD');
-    var thing4 = thurs.add(`3`, 'days').format('YYYY-MM-DD');
+    var thurs = moment(myDate, 'MM-DD');
+    var thing4 = thurs.add(`3`, 'days').format('MM-DD');
     console.log(thing4);
 
-    var fri = moment(myDate, 'YYYY-MM-DD');
-    var thing5 = fri.add(`4`, 'days').format('YYYY-MM-DD');
+    var fri = moment(myDate, 'MM-DD');
+    var thing5 = fri.add(`4`, 'days').format('MM-DD');
     console.log(thing5);
 
-    var sat = moment(myDate, 'YYYY-MM-DD');
-    var thing6 = sat.add(`5`, 'days').format('YYYY-MM-DD');
+    var sat = moment(myDate, 'MM-DD');
+    var thing6 = sat.add(`5`, 'days').format('MM-DD');
     console.log(thing6);
 
-    var sun = moment(myDate, 'YYYY-MM-DD');
-    var thing7 = sun.add(`6`, 'days').format('YYYY-MM-DD');
+    var sun = moment(myDate, 'MM-DD');
+    var thing7 = sun.add(`6`, 'days').format('MM-DD');
     console.log(thing7);
 
     
